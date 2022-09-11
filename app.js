829c234f33452ce10675504d2463035a0bd60b4a
Zouhair <zouhairsahtout66@gmail.com>
2022-09-11 14:10:12 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const slides = document.getElementsByClassName('carousel-item');
+const prevButton = document.getElementById('carousel-button-prev');
+const nextButton = document.getElementById('carousel-button-next');
+
+let slidePosition = 0;
+const totalSlides = slides.length;
+
+
+// Listen for a click
+nextButton.addEventListener('click', moveToNextSlide);
+prevButton.addEventListener('click', moveToPrevSlide);
+
+
+function hideAllSlides() {
+    for(const slide of slides) {
+        slide.classList.remove('carousel-item-visible');
+        slide.classList.add('carousel-item-hidden');
+    }
+}
+
+function moveToNextSlide() {
+    hideAllSlides();
+
+    if(slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+
+    slides[slidePosition].classList.add("carousel-item-visible");
+}
+
+function moveToPrevSlide() {
+    hideAllSlides();
+    if(slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition --;
+    }
+
+    slides[slidePosition].classList.add('carousel-item-visible');
+}
